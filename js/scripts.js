0c7290b36b8924013cd5ab35118afc0db0c1ae73
Ahmed Khokar and Skye Nguyen <ahmed@>
2018-08-14 14:13:42 -0700

    add backend logic to check for palindromes in a sentence

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,28 @@
 var reverseStr = function(string) {
-  var resultStr = string.split("").reverse().join("");
-  return resultStr;
+  if (string.includes(" ")) {
+    var noSpaceStr =string.split(" ").join("")
+    var resultStr = noSpaceStr.split("").reverse().join("").toUpperCase();
+    return resultStr;
+  }
+  else {
+    var resultStr = string.split("").reverse().join("").toUpperCase();
+    return resultStr;
+  }
 }
-
-
 $(document).ready(function() {
   $("form#input").submit(function(event){
     event.preventDefault();
-    var inputStr = $("#inputStr").val();
-    console.log(inputStr);
-    if (inputStr === reverseStr(inputStr)) {
-      $("#result").empty();
-    $("#result").append("a palindrome!");
-    }
-    else {
-      $("#result").empty();
-      $("#result").append("not a palindrome!");
-    }
+    var inputStr = $("#inputStr").val().split(" ").join("").toUpperCase();
+    console.log(reverseStr(inputStr));
+
+      if (inputStr === reverseStr(inputStr)) {
+        $("#result").empty();
+      $("#result").append("a palindrome!");
+      }
+      else {
+        $("#result").empty();
+        $("#result").append("not a palindrome!");
+      }
     $("#result").show();
   });
 });
